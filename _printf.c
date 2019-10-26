724a5cf7acfc35566992dca50a8862ccba89a2b2
darkares23 <desarrollojuseca@gmail.com>
2019-10-26 17:37:29 -0500

    added first files to prinf task

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: is a character string. The format string is
+ * composed of zero or more directives
+ *Returns: the number of characters printed
+ *(excluding the null byte used to end output to strings) 
+*/
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef PRINTF
+#define PRINTF
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+#endif
